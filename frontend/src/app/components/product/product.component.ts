4004a8fc1f2562dd2b7c46c1f8bbd40dde317ea2
Kurz <emanuel.kurz@it.htl-hl.ac.at>
2025-03-27 07:52:49 +0100

    created frontend

---
+++ frontend/src/app/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+
+@Component({
+  selector: 'app-product',
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+}
---
+++ frontend/src/app/services/product.service.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ProductService } from './product.service';
+
+describe('ProductService', () => {
+  let service: ProductService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService]
+    });
+    service = TestBed.inject(ProductService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should retrieve products from the API via GET', () => {
+    const dummyProducts = [
+      { id: '1', name: 'Product 1' },
+      { id: '2', name: 'Product 2' }
+    ];
+
+    service.getProducts().subscribe(products => {
+      expect(products.length).toBe(2);
+      expect(products).toEqual(dummyProducts);
+    });
+
+    const req = httpMock.expectOne(`${service['apiUrl']}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyProducts);
+  });
+
+  // Add more tests for other methods (getProduct, createProduct, updateProduct, deleteProduct) as needed
+});
