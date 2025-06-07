0f607eb6c6cc523666e735217d88dfebd01fc822
210135 <emanuel.kurz@it.htlhl.at>
2025-06-07 10:47:57 +0200

    styled frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProductComponent} from './components/product/product.component';
-import {HeadComponent} from './components/head/head.component';
+import {HeaderComponent} from './components/header/header.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductComponent, HeadComponent],
+  imports: [RouterOutlet, ProductComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
