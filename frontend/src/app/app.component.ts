19317c4e26d458023308fd5750b2183f8e68677a
210135 <emanuel.kurz@it.htlhl.at>
2025-06-07 11:06:06 +0200

    added basic functionality, products show in frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProductComponent} from './components/product/product.component';
-import {HeaderComponent} from './components/header/header.component';
-import {SidebarComponent} from './components/sidebar/sidebar.component';
+import { ProductComponent } from './components/product/product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductComponent, HeaderComponent, SidebarComponent],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ProductComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/components/product/product.component.ts
+++ frontend/src/app/components/product/product.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  stock: number;
+  demand: number;
+}
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
+  standalone: true
 })
-export class ProductComponent{
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
